d6511ddad72661bf644effffcec5bb97eb8cdab9
Mohamed El Amine Chafiki <sdadclub@protonmail.com>
2022-01-13 17:21:14 +0100

    added the whole project

---
+++ admin/database.php
@@ -0,0 +1,33 @@
+<?php
+
+
+ class Database 
+ {
+    private static $dbHost = "localhost";
+    private static $dbName="mamma_yummy";
+    private static $dbUser="yourUsername";
+    private static $dbUserPassword="yourPassword";
+
+    private static $connection = null ;
+
+    public static function connect()
+    {
+        try{
+            self::$connection = new PDO( "mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName , self::$dbUser , self::$dbUserPassword );
+        }
+        catch(PDOException $e){
+            die($e->getMessage());
+        }
+        return self::$connection;  
+        
+    }
+    public static function disconnect(){
+        self::$connection = null;
+    }
+}
+
+Database::connect();
+
+    
+
+?>
